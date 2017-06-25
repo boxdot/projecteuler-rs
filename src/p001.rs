f26ea4f01ab04b541082fc54163a7ee841286e12
boxdot <d@zerovolt.org>
2017-06-25 20:13:07 +0200

    Problem 1: constant time solution.

--- src/p001.rs
+++ src/p001.rs
@@ -1,5 +1,15 @@
-fn sum_of_mult_3_and_5(n: u64) -> u64 {
+#[allow(dead_code)]
+fn sum_of_mult_3_and_5_naive(n: u64) -> u64 {
     (1..n).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
+}
+
+fn sum_div_by(n: u64, p: u64) -> u64 {
+    let num_div_by = (n - 1) / p;
+    p * num_div_by * (num_div_by + 1) / 2
+}
+
+fn sum_of_mult_3_and_5(n: u64) -> u64 {
+    sum_div_by(n, 3) + sum_div_by(n, 5) - sum_div_by(n, 15)
 }
 
 pub fn solve() -> u64 {
@@ -8,10 +18,11 @@ pub fn solve() -> u64 {
 
 #[cfg(test)]
 mod test {
-    use super::sum_of_mult_3_and_5;
+    use super::*;
 
     #[test]
     fn test_sum_of_mult_3_and_5() {
+        assert!(23 == sum_of_mult_3_and_5_naive(10));
         assert!(23 == sum_of_mult_3_and_5(10));
     }
 }
